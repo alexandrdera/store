9c6bd5af9a5de1921d2d9fe047a30280bab8df0b
alexandrdera <alexandr.dera@gmail.com>
2014-03-11 20:16:39 +0200

    27

---
+++ init.rb
@@ -0,0 +1,18 @@
+require_relative "item_container"
+require_relative "cart"
+require_relative "order"
+require_relative "item"
+require_relative "virtual_item"
+require_relative "real_item"
+
+item1 = VirtualItem.new({ :price => 101, :weight =>100, :name => "Car" })
+item2 = RealItem.new({ :weight =>100, :price => 101, :name => "Car" })
+item3 = RealItem.new({ :weight =>100, :price => 10, :name => "Dishwasher" })
+
+cart = Cart.new
+cart.add_item item1
+cart.add_item item2
+cart.add_item item3
+
+puts cart.items.size
+
---
+++ order.rb
@@ -0,0 +1,16 @@
+class Order
+
+  attr_reader :items 
+  
+  include ItemContainer
+
+	def initialize
+		@items = Array.new
+    #...
+	end	
+
+  def place
+    #actualy place an order
+  end
+
+end
